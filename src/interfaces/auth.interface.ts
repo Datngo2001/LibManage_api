4a0dc077b2cd83810cca00dff06d837b4d7d0096
DatNgo <ngominhdat110115@gmail.com>
2022-03-31 11:03:44 +0700

    first commit

---
+++ src/interfaces/auth.interface.ts
@@ -0,0 +1,16 @@
+import { Request } from 'express';
+import { User } from '@prisma/client';
+
+export interface DataStoredInToken {
+  id: number;
+  role: string;
+}
+
+export interface TokenData {
+  token: string;
+  expiresIn: number;
+}
+
+export interface RequestWithUser extends Request {
+  user: User;
+}
