18e22edff0100a7e3ac403661546142453dbb7f1
DatNgo <ngominhdat110115@gmail.com>
2022-04-03 17:33:18 +0700

    login

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -57,7 +57,7 @@ class AuthService {
   public createToken(user: User): TokenData {
     const dataStoredInToken: DataStoredInToken = { id: user.id, role: user.roleName };
     const secretKey: string = SECRET_KEY;
-    const expiresIn: number = 60 * 60;
+    const expiresIn: number = 60 * 60 * 60;
 
     return { expiresIn, token: sign(dataStoredInToken, secretKey, { expiresIn }) };
   }
